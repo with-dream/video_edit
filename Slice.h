3328ae145d9cd431370b6865566fc81d1aaadedb
with-dream <854415002@qq.com>
2021-02-17 02:27:33 +0800

    first commit

---
+++ Slice.h
@@ -0,0 +1,18 @@
+//
+// Created by ms on 2021/2/16.
+//
+
+#ifndef VIDEO_EDIT_SLICE_H
+#define VIDEO_EDIT_SLICE_H
+
+
+class Slice {
+private:
+
+public:
+    int slice(const char *, long start, long end);
+
+};
+
+
+#endif //VIDEO_EDIT_SLICE_H
---
+++ main.cpp
@@ -0,0 +1,9 @@
+
+#include "Slice.h"
+
+int main(int argc, char *argv[]) {
+    Slice sp;
+    sp.slice("/Users/ms/Desktop/tt.mp4", 0, 0);
+    return 0;
+}
+
